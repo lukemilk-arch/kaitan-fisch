4338fd07a846d90619212530a2f8d94c8a5febbf
lukemilk-arch <lukiprawat@gmail.com>
2025-09-26 14:21:26 +0700

    fisch.lua

---
+++ fisch.lua
@@ -0,0 +1,28 @@
+getgenv().Configuration = {
+        Enabled = true,
+        ZoneFish = {
+            ['Zone'] = 'Crystal lsle', 
+            ['Event Zone Fishing'] = false, 
+        },
+        CollectMeteor = false,  
+        EquipRod = 'Auto',
+        Rods = {
+            ['Trident Rod'] = true,
+            ['Aurora Rod'] = true,
+            ['Kings Rod'] = true,
+            ['Sunken Rod'] = true,
+            ['Mythical Rod'] = true, 
+            ['Rod Of The Depths'] = true,
+            ['Destiny Rod'] = true,
+        },
+        AutoSell = {
+            ['Enabled'] = true,
+            ['Sell every'] = 200,
+            ['Mythical'] = true,
+            ['Legendary'] = true,
+            ['Enchant Rellcs'] = true,
+            ['Exotic'] = true,
+            ['Event Fish'] = true,
+            ['Limited Fish'] = true,
+        },
+    }
